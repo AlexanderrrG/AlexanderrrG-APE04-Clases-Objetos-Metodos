36aa4298d836d811a85e643f3d33ee471b58c5ab
AlexanderG. <guamanalexander807@gmail.com>
2026-05-19 18:21:15 -0500

    Update Estudiante.java

--- Java/Estudiante.java
+++ Java/Estudiante.java
@@ -1,12 +1,13 @@
 
+
 public class Estudiante {
-    
+    // Atributos privados
     private String cedula;
     private String nombre;
     private String apellido;
     private double nota1, nota2, nota3;
 
-    
+    // Constructor
     public Estudiante(String cedula, String nombre, String apellido, double nota1, double nota2, double nota3) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -16,7 +17,7 @@ public class Estudiante {
         this.nota3 = nota3;
     }
 
-    
+    // Métodos get y set
     public String getCedula() { return cedula; }
     public void setCedula(String cedula) { this.cedula = cedula; }
 
@@ -32,12 +33,12 @@ public class Estudiante {
         this.nota3 = n3;
     }
 
-    
+    // Método para calcular el promedio
     public double calcularPromedio() {
         return (nota1 + nota2 + nota3) / 3.0;
     }
 
-   
+    // Método para determinar si aprueba o reprueba
     public String determinarEstado() {
         if (calcularPromedio() >= 7.00) {
             return "Aprobado";
@@ -46,7 +47,7 @@ public class Estudiante {
         }
     }
 
-    
+    // Método para mostrar la información del estudiante
     public void mostrarInformacion() {
         System.out.println("\nCedula: " + cedula + " | Nombre: " + nombre + " " + apellido);
         System.out.println("Notas: " + nota1 + ", " + nota2 + ", " + nota3);
